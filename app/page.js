03ee966ff736dc08cbd7eda53fbbb8f7b3d2a986
Luke Wheeler <lukewheeleruk@gmail.com>
2024-02-07 00:48:04 +0000

    removed console.log

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import { getInitialData } from "@/app/lib/data";
 import PlayerList from "@/app/ui/player-list";
 
 export default async function Home({ searchParams }) {
-  console.log(searchParams);
   const { initialPlayers, initialCursor } = await getInitialData(searchParams);
   const initialTeam = searchParams?.team || null;
   return (
