d6324085a96d2a83ac5aae476402873254ea4c36
Luke Wheeler <lukewheeleruk@gmail.com>
2025-09-08 19:03:14 +0100

    added button for code (non-functional as of yet)

--- app/page.js
+++ app/page.js
@@ -1,19 +1,14 @@
 import HomePage from "@/app/ui/HomePage";
 import { getPlayers } from "@/app/lib/data";
 
-//this should work
-
 export default async function Page({ searchParams }) {
-  // Await searchParams to make it safe
   const params = await searchParams;
 
-  // Now build initial filters
   const initialFilters = {
     team: params.team ?? null,
     tags: params.tags ?? null,
   };
 
-  // Fetch initial players
   const { players: initialPlayers, cursor: initialCursor } = await getPlayers(
     initialFilters
   );
--- app/ui/Player.js
+++ app/ui/Player.js
@@ -1,10 +1,12 @@
 import { formatCode } from "@/app/lib/formatting";
 import TeamBadge from "@/app/ui/TeamBadge";
 import { Badge } from "@/components/ui/badge";
+import { Copy } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Player({ username, code, team, tags, message, level }) {
   return (
-    <div className="flex flex-col gap-2 py-4 px-6 border-b">
+    <div className="flex flex-col gap-2 p-6 border-b">
       <div className="flex gap-2">
         <h2 className="text-xl font-medium">{username}</h2>
         <TeamBadge team={team} level={level} />
@@ -18,7 +20,15 @@ export default function Player({ username, code, team, tags, message, level }) {
       </div>
       <p className="pt-2 text-gray-600">{message}</p>
       <div className="flex flex-row-reverse">
-        <h4 className="font-bold font-mono text-xl">{formatCode(code)}</h4>
+        <Button
+          variant="outline"
+          size="lg"
+          onClick={null}
+          className="flex items-center gap-4 font-mono"
+        >
+          <Copy className="w-5 h-5" strokeWidth={3} />
+          <h4 className="font-medium font-mono text-xl">{formatCode(code)}</h4>
+        </Button>
       </div>
     </div>
   );
