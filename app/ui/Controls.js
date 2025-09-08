fd7059a8ccb7e74eaafc54add7b1c74a5d74337e
Luke Wheeler <lukewheeleruk@gmail.com>
2025-09-08 18:34:16 +0100

    made sidebar and ad area sticky

--- app/ui/Controls.js
+++ app/ui/Controls.js
@@ -5,7 +5,7 @@ import Filters from "./Filters";
 
 export default function Controls({ filters, setFilters, submitForm }) {
   return (
-    <div className="flex flex-col p-6 gap-12 items-baseline lg:flex-none lg:w-60">
+    <div className="flex flex-col p-6 gap-12 items-baseline lg:sticky lg:top-0 lg:h-screen lg:flex-none lg:w-60">
       <AddProfileDialog
         onProfileAdded={submitForm}
         resetFilters={() => setFilters({ team: null, tags: null })}
--- app/ui/SkyscraperAdArea.js
+++ app/ui/SkyscraperAdArea.js
@@ -1,6 +1,6 @@
 export default function SkyscraperAdArea() {
   return (
-    <div className="hidden lg:block flex flex-none p-8">
+    <div className="hidden flex flex-none p-8 lg:block lg:sticky lg:top-0 lg:h-screen">
       <div className="bg-gray-200 w-[160px] h-[600px] mx-auto flex items-center justify-center">
         <span className="text-gray-500">Ad Space</span>
       </div>
