913e5a8cc99f53b4fe80f2cc778926c746ab36b1
Luke Wheeler <lukewheeleruk@gmail.com>
2025-09-16 16:25:20 +0100

    added avatar area to Player.js

--- app/ui/Player.js
+++ app/ui/Player.js
@@ -4,26 +4,29 @@ import PlayerControlButtons from "@/app/ui/PlayerControlButtons";
 
 export default function Player({ username, code, team, tags, message, level }) {
   return (
-    <div className="flex flex-col gap-2 p-6 border-b">
-      <div className="flex gap-2">
-        <h2 className="text-2xl font-bold">{username}</h2>
-        <TeamBadge team={team} level={level} />
-      </div>
-      <div className="flex gap-2">
-        {tags?.map((tag) => (
-          <div className="text-sm uppercase" key={tag}>
-            <Badge variant="secondary">{tag}</Badge>
-          </div>
-        ))}
-      </div>
-      <p className="pt-2 text-gray-600">{message}</p>
-      <div className="flex flex-row-reverse gap-4 mt-2">
-        <PlayerControlButtons
-          code={code}
-          username={username}
-          team={team}
-          level={level}
-        />
+    <div className="flex flex-row p-6 border-b gap-4">
+      <div className="flex w-32 bg-gray-200"></div>
+      <div className="flex flex-1 flex-col gap-2">
+        <div className="flex gap-2">
+          <h2 className="text-2xl font-bold">{username}</h2>
+          <TeamBadge team={team} level={level} />
+        </div>
+        <div className="flex gap-2">
+          {tags?.map((tag) => (
+            <div className="text-sm uppercase" key={tag}>
+              <Badge variant="secondary">{tag}</Badge>
+            </div>
+          ))}
+        </div>
+        <p className="pt-2 text-gray-600">{message}</p>
+        <div className="flex flex-row-reverse gap-4 mt-2">
+          <PlayerControlButtons
+            code={code}
+            username={username}
+            team={team}
+            level={level}
+          />
+        </div>
       </div>
     </div>
   );
